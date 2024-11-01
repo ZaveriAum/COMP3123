2ae3c500a71c1916f68b62f9218a06f17241a355
Aum Zaveri <aumzaveri06@gmail.com>
2024-11-01 10:59:21 -0400

    week 9 lab 7

---
+++ labweek09/src/Main.js
@@ -0,0 +1,15 @@
+import './Main.css'
+
+function main() {
+    return (
+        <div className="main">
+            <h1 className='div_item'>Welcome to Fullstack Development - 1</h1>
+            <h2 className='div_item'>React JS Programming Week09 Lab exercise</h2>
+            <h3 className='div_item'>101413047</h3>
+            <h4 className='div_item'>Aum Zaveri</h4>
+            <h5 className='div_item'>George Brown College, Toronto</h5>
+        </div>
+    )
+}
+
+export default main
